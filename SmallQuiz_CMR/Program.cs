8f3d2ea289103eb0e97f6e6b1590815f4ad889db
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-29 16:53:14 +0200

    Add project files.

---
+++ SmallQuiz_CMR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallQuiz_CMR
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string cmrFirstPresident = "Amadou Ahidjo";
+			string cmrCurrentPresident = "Paul Biya";
+			string[] cmrTowns = { "Buea", "Kribi", "Limbe", "Douala", "Yaounde", "Bafoussam", "Bamenda", "Eseka", "Mbouda", "Edea" };
+
+			Console.WriteLine("\n\t-- CAMEROON QUIZ --\n\n");
+			Console.Write("");
+
+
+		}
+	}
+}
